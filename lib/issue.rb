1edeedc0acbb48efd539142648a62e4fe08bc88a
Jerod Santo <jerod.santo@gmail.com>
2017-03-29 15:23:19 -0500

    add JS Party, $ustain promotions

--- lib/issue.rb
+++ lib/issue.rb
@@ -5,7 +5,7 @@ class Issue
   attr_reader :date, :promotions, :top_new, :top_all
   def initialize date, json
     @date = date
-    @promotions = %w(podcast feedback rfc gotime spotlight master).sample 2
+    @promotions = %w(podcast rfc gotime spotlight master jsparty sustain).sample 2
     process_top_new json
     process_top_all json
   end
