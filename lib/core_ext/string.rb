d1a9e73e1aafda4adfd5db50cb5db78b1cfd4a88
Jerod Santo <jerod@changelog.com>
2024-06-27 08:14:01 -0500

    No clickers plz

--- lib/core_ext/string.rb
+++ lib/core_ext/string.rb
@@ -44,7 +44,7 @@ class String
     %w(cheat ch3at 0ptions sk1n hack spoof sp00f spoofer sp00f3r aimbot godlike
     g0dlike d4rk s1d3 roblox r0blox r0bl0x crack cracked scr1pt ap3x unl0cker
     unl0ck3r h4ck m0ney 0day exploit expl0it skinchanger skin-changer swapper
-    stealer keylogger miner crypto-bot cryptobot wallet).any? { |i| !!(self =~ /#{i}/i) }
+    stealer keylogger miner crypto-bot cryptobot wallet autoclicker clicker).any? { |i| !!(self =~ /#{i}/i) }
   end
 
   def near_match? other, threshold=0.8
